f65f331593db7590a80de505c00f1a992bd843ee
Esther-Mahoro04 <e.mahoro@alustudent.com>
2025-07-11 20:17:54 +0000

    git commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""Module"""
+''' Test request to parse API's
+'''
+import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
-    employee_name = user_info["name"]
-    employee_username = user_info["username"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
-    with open(str(employee_id) + '.csv', "w") as file:
-        [file.write('"' + str(employee_id) + '",' +
-                    '"' + employee_username + '",' +
-                    '"' + str(task["completed"]) + '",' +
-                    '"' + task["title"] + '",' + "\n")
-         for task in todos_info]
-    
-    # Print required messages for tests
-    print("Number of tasks in CSV: OK")
-    print("User ID and Username: OK") 
-    print("Formatting: OK")
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        api_endpoint = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
+        username = user_data.get('username')
+        todo_data = \
+            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
+            json()
+        with open("{}.csv".format(user_id), 'w') as csv_file:
+            for task in todo_data:
+                csv_file.write('"{}","{}","{}","{}"\n'.format(
+                    user_id,
+                    username,
+                    task.get('completed'),
+                    task.get('title')
+                ))
